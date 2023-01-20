9a74b80912c3c82e293bfc89b335731ee746f964
Anandito <nandoaja901@gmail.com>
2023-01-21 04:26:19 +0700

    Database Complete

---
+++ app/Http/Controllers/PaketSatuController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\paketSatu;
+use Illuminate\Http\Request;
+
+class PaketSatuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\paketSatu  $paketSatu
+     * @return \Illuminate\Http\Response
+     */
+    public function show(paketSatu $paketSatu)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\paketSatu  $paketSatu
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(paketSatu $paketSatu)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\paketSatu  $paketSatu
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, paketSatu $paketSatu)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\paketSatu  $paketSatu
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(paketSatu $paketSatu)
+    {
+        //
+    }
+}
